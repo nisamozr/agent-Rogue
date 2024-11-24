1631097e99d772f4a4a9e966723a7660434badcc
nisamozr <codeexplorkl10@gmail.com>
2024-11-24 13:43:25 +0530

    agent charecter

--- src/components/Sidebar/TeerminalBox.tsx
+++ src/components/Sidebar/TeerminalBox.tsx
@@ -31,7 +31,7 @@ const TeerminalBox = () => {
   const [topic, setTopic] = useState(""); // Default amount
   const connection = new Connection(import.meta.env.VITE_SOL_RPC);
 
-  const amount = BigInt(20 * 10 ** 6);
+  const amount = BigInt(20000 * 10 ** 6);
   // const { connection } = useConnection();
   const { toast } = useToast();
   const { disableAction, setDisableAction } = useAppCtx();
